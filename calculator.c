45a47334841c2431e27a50dadd4ea42ddb70fb4b
Iretevin <iretevin@gmail.com>
2022-05-10 11:52:39 +0100

    cal

---
+++ calculator.c
@@ -0,0 +1,53 @@
+//WRITTEN BY IRETE VINCENT//
+//THIS IS A SIMPLE CALCULATOR PROGRAM USING SWITCH IN C LANGUAGE//
+//THIS CALCULATOR DOES THESE OPERATIONS//
+//1:ADDITION OF TWO NUMBERS 2:SUBTRACTION OF TWO NUMBERS//
+//3:DIVISION OF TWO NUMBERS 4:MULTIPLICATION OF TWO NUMBERS//
+#include<stdio.h>
+int main(){
+float num1,num2,result;        
+char operator;('+','-','*','%');
+printf("Please enter an operator:");
+scanf("%c",& operator);
+printf("Please enter two operands:\n");
+scanf("%f%f",&num1,&num2);
+
+switch (operator)    
+{
+case '+':
+result=num1+num2;
+printf("sum=%f\n,",result);
+break;
+
+case '-':
+result=num1-num2;
+printf("subtraction=%f\n",result);
+break;
+
+case '%':
+result=num1/num2;
+printf("division=%f\n",result);
+break;
+
+case'*':
+result=num1*num2;
+printf("multiplication=%f\n",result);
+break;
+
+default:
+printf("you have not selected the right operator");
+break;
+}
+
+   
+    return 0;
+}        
+          
+    
+    
+    
+    
+
+
+
+
