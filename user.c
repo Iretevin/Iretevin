2ec782db3395137e1e245ded419268dc77719d96
Iretevin <iretevin@gmail.com>
2022-05-11 14:53:49 +0100

    adding user.c

---
+++ user.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include"userheader.h"
+
+int main()
+{
+    float num1,num2;
+    
+    
+    
+    printf("enter two numbers\n");
+    
+    scanf("%f%f",&num1,&num2);
+    
+    printf("\n sum=%f",(num1+num2));
+    
+    printf("\n division=%f",(num1/num2));
+    
+    printf("\n sub=%f",(num1-num2));
+    
+    printf("\n product=%f",(num1*num2));
+    
+    
+}
